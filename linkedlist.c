0fe35285e58de4723d3c67d6d650959ac7853a35
Siddharth Sharma <40432616+sidoncode@users.noreply.github.com>
2019-10-23 02:05:43 +0530

    commit for the linkedlist

--- linkedlist.c
+++ linkedlist.c
@@ -2,6 +2,42 @@
 #include<stdlib.h>
 #include<conio.h>
 
+struct Node{
+    int data;
+    struct Node* link;
+};
+struct Node *first =NULL;
+
+void insert_at_begin(){
+    struct Node *t; int x;
+    printf("Enter The data \n");
+    scanf("%d",&x);
+    t = (struct Node*)malloc(sizeof(struct Node));
+    if(first == NULL){
+        first = t;
+        first->data = x;
+        first->link = NULL;
+    }
+    else{
+        t->data = x;
+        t->link = first;
+        first = t;
+    }
+}
+
+void insert_at_end(){
+    struct Node* t;
+    int x;
+    printf("Enter the data\n");
+    scanf("%d",&x);
+    t = (struct Node*)malloc(sizeof(struct Node));
+    if(first == NULL){
+        first = t;
+        first->data = x;
+        first->link = NULL;
+    }
+
+}
 
 
 
