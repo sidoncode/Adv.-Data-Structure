8c2c9c033132d347ef04f72f715af3fd143e8b16
Siddharth Sharma <40432616+sidoncode@users.noreply.github.com>
2019-10-23 02:31:40 +0530

    commits for the last fx in the main fx

--- linkedlist.c
+++ linkedlist.c
@@ -8,6 +8,10 @@ struct Node{
 };
 struct Node *first =NULL;
 
+
+int count =0;
+
+
 void insert_at_begin(){
     struct Node *t; int x;
     printf("Enter The data \n");
@@ -36,14 +40,70 @@ void insert_at_end(){
         first->data = x;
         first->link = NULL;
     }
-
+    else{
+        struct Node* p;
+        p = first;
+        while (p != NULL){
+            p = p->link;
+        }
+        p->link = t;
+        p->data = x;
+        t->link = NULL;
+    }
 }
-
-
-
-
-
-
+void traverse(){
+    struct Node* p;
+    p = first;
+    if(p == NULL){
+        printf("The linkedlist is empty\n");
+    }
+    else{
+        while (p != NULL){
+            printf("The datas are %d",p->data);
+            p = p->link;
+        }
+    }
+}
+void delete_from_begin() {
+   struct node *t;
+   int n;
+   if (first == NULL) {
+      printf("Linked list is already empty.\n");
+      return;
+   }
+   
+   n = first->data;
+   t = first->link;
+   free(first);
+   first = t;
+   count--;
+   printf("%d deleted from beginning successfully.\n", n);
+}
+void delete_from_end() {
+   struct Node *t, *u;
+   int n; 
+   if (first == NULL) {
+      printf("Linked list is already empty.\n");
+      return;
+   }
+   count--;
+   if (first->link == NULL) {
+      n = first->data;
+      free(first);
+      first = NULL;
+      printf("%d deleted from end successfully.\n", n);
+      return;
+   }
+   t = first;
+   while (t->link != NULL) {
+      u = t;
+      t = t->link;
+   }
+   n = t->data;
+   u->link = NULL;
+   free(t);
+   printf("%d deleted from end successfully.\n", n);
+}
 int main(){
 
 
