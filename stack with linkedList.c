1c27c82ffd898925fd22f6f0ce22446f33e28a0c
Siddharth Sharma <40432616+sidoncode@users.noreply.github.com>
2019-11-24 15:55:50 +0530

    new algo for ll with stack

---
+++ stack with linkedList.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+
+struct stack{
+    int data;
+    struct stack  *link;
+};
+struct stack *top = NULL;
+
+void push(){
+    struct stack *t;
+    t = (struct stack*)malloc(sizeof(struct stack));
+    printf("Enter data");
+    scanf("%d",&t->data);
+    t->link = top;
+    top = t;
+}
+
+void pop(){
+    struct stack *t;
+    if(top == NULL){
+        printf("No element\n");
+    }
+    else{
+        /* code */
+        t = top;
+        top  = top ->link;
+    }
+    
+}
+void display(){
+    while (top != NULL){
+       printf("%d",top->data);
+       top = top->link;
+    }
+}
+
+int main(){
+    /*push(10);
+    push(20);
+    push(30);
+    push(40);
+    push(50);
+    push(60);*/
+    push();
+    push();
+    display();
+
+getch();
+return 0; 
+}
