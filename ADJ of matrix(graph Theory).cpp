d013a6fbb37d4428281e12f74b11aa0abcf877e9
Siddharth Sharma <40432616+sidoncode@users.noreply.github.com>
2020-01-20 22:37:29 +0530

    using malloc

--- ADJ of matrix(graph Theory).cpp
+++ ADJ of matrix(graph Theory).cpp
@@ -1,5 +1,6 @@
 #include<stdlib.h>
 #include<stdio.h>
+#include<conio.h>
 
 struct graph{
     int v;
@@ -7,7 +8,28 @@ struct graph{
     int** adj; //double pointer for the storage 2d mat //
 };
 
+struct graph *g;
+
+struct graph *adj_matrix_graph(){
+    int i;
+    int u,v;
+    struct graph *g = (struct graph*)malloc(sizeof(struct graph));
+    // creating the matrix of adj //
+    printf("Enter the number of vertices and edges\n");
+    scanf("%d %d",&g->v,&g->e);
+    g->adj =(int**) malloc(sizeof(g->v * g->v)); 
+    for(i=0;i<g->v;i++){
+        scanf("%d %d",&u,&v);
+        g->adj[u][v]=1;
+        g->adj[v][u]=1;
+    }
+    return g;
+}
+
+
 
 int main(){
-
+ adj_matrix_graph();
+    getch();
+    return 0;
 }
